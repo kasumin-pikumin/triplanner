e1311d993fb5f4d4703ed42c3e41acab231ef083
EC2 Default User <ec2-user@ip-172-31-0-41.ap-northeast-1.compute.internal>
2023-07-26 13:09:46 +0000

    application_controllerの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ before_action :configure_permitted_parameters, if: :devise_controller?
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_image, :name, :birth_of_date])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:profile_image, :name, :birth_of_date])
   end
 
 end
